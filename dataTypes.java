18d7e4585055ea64b8b4a3a502ae628ec5599de1
abhishek1704 <velankarabhishek@gmail.com>
2020-07-01 10:51:00 +0530

    Add files via upload

---
+++ dataTypes.java
@@ -0,0 +1,13 @@
+public class dataTypes
+{
+	public static void main(String[] args)
+	{
+		byte byteNo = 25;
+		short shortNo = 500;
+		int intNo = 2000;
+		long lNo = 50000 + 10*(byteNo + shortNo + intNo);;
+		
+		
+		System.out.println(lNo);
+	}
+}
